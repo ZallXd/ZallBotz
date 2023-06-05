2fb2fee2b25bcd56c40f153f6723f121e5f68be4
ZallXd <100450216+ZallXd@users.noreply.github.com>
2023-06-05 17:13:32 +0700

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -14,13 +14,13 @@ export async function before(m, {conn, isAdmin, isBotAdmin }) {
             const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
             if (m.text.includes(linkThisGroup)) return !0
         }
-        await conn.reply(m.chat, `*≡ Enlace Detectado*
+        await conn.reply(m.chat, `*≡ ʟɪɴᴋ ᴛᴇʀᴅᴇᴛᴇᴋsɪ*
             
-No permitimos enlaces de otros grupos 
-lo siento *@${m.sender.split('@')[0]}*  serás expulsado del grupo ${isBotAdmin ? '' : '\n\nNo soy admin así que no te puedo expulsar :"v'}`, null, { mentions: [m.sender] } )
+*ᴀᴘᴀᴀɴ ᴄᴏʙᴀ*
+*ʜᴀʟʟᴏ* *@${m.sender.split('@')[0]}*  *ɴᴏ sʜᴀʀᴇ ʟɪɴᴋ ᴡɪʀ 乂* ${isBotAdmin ? '' : '\n\n*ᴊᴀᴅɪᴋᴀɴ ᴀᴅᴍɪɴ ᴛᴇʀʟᴇʙɪʜ ᴅᴀʜᴜʟᴜ ᴊᴀᴡɪʀ*'}`, null, { mentions: [m.sender] } )
         if (isBotAdmin && chat.antiLink) {
         	await conn.sendMessage(m.chat, { delete: m.key })
-            await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+            await conn.sendMessage(m.chat, { delete: m.key })
         } else if (!chat.antiLink) return //m.reply('')
     }
     return !0
