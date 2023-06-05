209b2bdea55893ae7ca4a1ef1a757c5d7cc8cc5b
ZallXd <100450216+ZallXd@users.noreply.github.com>
2023-06-05 17:14:47 +0700

    Update _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -12,7 +12,7 @@ let _muptime
       }) * 1000
     }
     let muptime = clockString(_muptime)
-		let bio = `\n🟢 Tiempo Activo ${muptime}\n\n ┃ 💎  By FG98`
+		let bio = `\nᴀᴋᴛɪғ sᴇʟᴀᴍᴀ ${muptime}\n\n ┃  ʙʏ ᴢᴀʟʟ sᴛᴏʀᴇ`
 		await this.updateProfileStatus(bio).catch(_ => _)
 		setting.status = new Date() * 1
 
@@ -24,5 +24,5 @@ function clockString(ms) {
   let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [d, ' Día(s) ️', h, ' Hora(s) ', m, ' Minuto(s)'].map(v => v.toString().padStart(2, 0)).join('')
+  return [d, ' Hari(s) ️', h, ' Jam(s) ', m, ' Menit(s)'].map(v => v.toString().padStart(2, 0)).join('')
 }
